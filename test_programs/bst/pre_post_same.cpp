a67f176ff5aa6fd66ef7896e625018e86a71e23c
Stonky-Boi <arnav63366@gmail.com>
2025-10-28 15:26:20 +0530

    minor fix

--- test_programs/bst/pre_post_same.cpp
+++ test_programs/bst/pre_post_same.cpp
@@ -6,9 +6,13 @@ int main()
     bt::BinaryTree<int> t1;
     t1.root = new bt::Node<int>(1);
     t1.root->left = new bt::Node<int>(2);
-    t1.root->right = new bt::Node<int>(3);
+    t1.root->left->left = new bt::Node<int>(3);
+
+    std::cout << "Tree 1 Preorder: ";
     t1.preorder(t1.root);
     std::cout << std::endl;
+
+    std::cout << "Tree 1 Postorder: ";
     t1.postorder(t1.root);
     std::cout << std::endl;
 
@@ -16,8 +20,12 @@ int main()
     t2.root = new bt::Node<int>(1);
     t2.root->left = new bt::Node<int>(2);
     t2.root->left->right = new bt::Node<int>(3);
+
+    std::cout << "Tree 2 Preorder: ";
     t2.preorder(t2.root);
     std::cout << std::endl;
+
+    std::cout << "Tree 2 Postorder: ";
     t2.postorder(t2.root);
     std::cout << std::endl;
 
